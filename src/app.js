9fe40ca886246e4092877f56c56e779774976a8f
severin05 <straeulis@bzz.ch>
2023-12-12 14:19:07 +0100

    Lösch Button für die Menü Seite hinzugefügt

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const model = require('./models');
@@ -8,6 +9,29 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Route zum Löschen eines Menüs
+app.post('/delete-menu', async (req, res) => {
+  try {
+    const menuIdToDelete = req.body.menuId;
+    // Überprüfen, ob das Menü in einem Speiseplan verwendet wird
+    const isUsedInSpeiseplan = await model.isMenuUsedInSpeiseplan(menuIdToDelete);
+
+    if (isUsedInSpeiseplan) {
+      return res.status(400).send('Das Menü kann nicht gelöscht werden, da es noch in einem Speiseplan verwendet wird.');
+    }
+
+    // Menü löschen, wenn es nicht in einem Speiseplan verwendet wird
+    await model.deleteMenu(menuIdToDelete);
+    res.redirect('/menus');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.get('/speiseplaene', async (req, res) => {
   const speiseplaene = await model.getAllSpeiseplaene();
--- src/models.js
+++ src/models.js
@@ -61,6 +61,35 @@ class CantinaModel {
       });
     });
   }
+
+
+static deleteMenu(menuId) {
+  return new Promise((resolve, reject) => {
+    const query = `DELETE FROM menus WHERE menu_id = ${menuId}`;
+    connection.query(query, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+  static isMenuUsedInSpeiseplan(menuId) {
+    return new Promise((resolve, reject) => {
+      const query =`SELECT COUNT(*) AS count FROM speiseplan WHERE mo1 = ${menuId} OR mo2 = ${menuId} OR mo3 = ${menuId} OR mo4 = ${menuId} OR di1 = ${menuId} OR di2 = ${menuId} OR di3 = ${menuId} OR di4 = ${menuId} OR mi1 = ${menuId} OR mi2 = ${menuId} OR mi3 = ${menuId} OR mi4 = ${menuId} OR do1 = ${menuId} OR do2 = ${menuId} OR do3 = ${menuId} OR do4 = ${menuId} OR fr1 = ${menuId} OR fr2 = ${menuId} OR fr3 = ${menuId} OR fr4 = ${menuId}`;
+
+      connection.query(query, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve( results[0].count > 0);
+        }
+      });
+    });
+
+}
 }
 
 module.exports = CantinaModel;
